b402b088ef413b1e1cfb6db390595de3bd89edf0
Hansen Limanta <hansenlimanta@gmail.com>
2022-10-11 22:36:13 +0700

    added postgres

--- config/database.php
+++ config/database.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL=parse_url('postgres://ewkipfwpqgpwvu:879492fb3b894e56352028c90852a57df2889d2a01e1e60255c9c292e6f44a61@ec2-23-20-140-229.compute-1.amazonaws.com:5432/dcblecfcinebpi
+C:\Hansen\Binus\7-1 Web Programming\Laravel\formapp');
 
 return [
 
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
